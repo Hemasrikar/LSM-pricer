ec1c770965f690d49719061c82822fc2e6a0fdfd
Hema Srikar <hemasrikar.ankem@gmail.com>
2026-03-01 15:51:18 +0000

    Added:Constant Basis Fucntion header

--- src/basis_functions.hpp
+++ src/basis_functions.hpp
@@ -1,1 +1,16 @@
 #pragma once
+
+#include "lsm_types.hpp"
+#include <cmath>
+#include <string>
+#include <stdexcept>
+
+
+//  ConstantBasis  
+// - intercept term; evaluates to 1.0 for any given values
+
+class ConstantBasis : public BasisFunction {
+public:
+    double evaluate(double x) const;
+    std::string name() const;
+};
