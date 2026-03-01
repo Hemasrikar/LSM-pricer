af0306c739b204061f352c63f4cb7d40f6003544
Hema Srikar <hemasrikar.ankem@gmail.com>
2026-03-01 16:03:02 +0000

    Added:Basis Function

---
+++ src/lsm_types.hpp
@@ -0,0 +1,11 @@
+
+
+//  BasisFunction  
+// — Abstract: strategy for regression basis
+
+class BasisFunction {
+public:
+    virtual ~BasisFunction() {}
+    virtual double evaluate(double x) const = 0;
+    virtual std::string name() const = 0;
+};
