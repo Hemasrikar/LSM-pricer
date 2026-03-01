f199a8607b8c3b4dd78ab8af19382a91a41ababc
Hema Srikar <hemasrikar.ankem@gmail.com>
2026-03-01 17:33:52 +0000

    Added:Monomial Basis Function

--- src/basis_functions.cpp
+++ src/basis_functions.cpp
@@ -8,3 +8,20 @@ double ConstantBasis::evaluate(double /*x*/) const { return 1.0; }
 std::string ConstantBasis::name() const { return "Const"; }
 
 }
+
+// Monomial Basis Function
+
+MonomialBasis::MonomialBasis(int power) {
+    if (power < 0) {
+        throw std::invalid_argument("power must be >= 0");
+    }
+    power_ = power;
+}
+
+double MonomialBasis::evaluate(double x) const {
+    return std::pow(x, power_);
+}
+
+std::string MonomialBasis::name() const {
+    return "x^" + std::to_string(power_);
+}
