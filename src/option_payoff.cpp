a9ff753021941a6ca32338d94847a57df3027ce9
Bobbywsv <bobbywsv@gmail.com>
2026-03-01 17:04:04 +0000

    Feature Added:Payoff Functions

--- src/option_payoff.cpp
+++ src/option_payoff.cpp
@@ -3,6 +3,10 @@
 
 #include "option_payoff.hpp"
 
+// Payoff functions of both put and call options
+// K = strike price
+// S = underlying price
+
 class Put_payoff 
 {
 private:
@@ -31,6 +35,22 @@ public:
         return std::max(S - K, 0.0);
     }
 };
+
+// Functions to check if the call and put payoff functions are in-the-money or out-the-money
+// Use bool to check if the payoff functions is in-the-money
+// Otherwise, it will return as false which implies it is out-the-money
+
+
+bool InTheMoney_Put(double S, double K) 
+{
+    return S < K;
+}
+
+bool InTheMoney_Call(double S, double K) 
+{
+    return S > K;
+}
+
 
 
 
