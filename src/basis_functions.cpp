87f458fec064ed1ae605a80bca3ced176389b0f9
Hema Srikar <hemasrikar.ankem@gmail.com>
2026-03-02 03:44:02 +0000

    Reformatted:Syntax

--- src/basis_functions.cpp
+++ src/basis_functions.cpp
@@ -1,27 +1,40 @@
 #include "basis_functions.hpp"
 #include <cmath>
 
-namespace lms{
+namespace lms {
 
-//  ConstantBasis
-double ConstantBasis::evaluate(double /*x*/) const { return 1.0; }
-std::string ConstantBasis::name() const { return "Const"; }
-
-}
-
-// Monomial Basis Function
-
-MonomialBasis::MonomialBasis(int power) {
-    if (power < 0) {
-        throw std::invalid_argument("power must be >= 0");
+// ConstantBasis
+class ConstantBasis : public BasisFunction {
+public:
+    double evaluate(double x) const {
+        return 1.0;
     }
-    power_ = power;
-}
 
-double MonomialBasis::evaluate(double x) const {
-    return std::pow(x, power_);
-}
+    std::string name() const {
+        return "Const";
+    }
+};
 
-std::string MonomialBasis::name() const {
-    return "x^" + std::to_string(power_);
+// MonomialBasis
+class MonomialBasis : public BasisFunction {
+public:
+    MonomialBasis(int power) {
+        if (power < 0) {
+            throw std::invalid_argument("power must be >= 0");
+        }
+        power_ = power;
+    }
+
+    double evaluate(double x) const {
+        return std::pow(x, power_);
+    }
+
+    std::string name() const {
+        return "x^" + std::to_string(power_);
+    }
+
+private:
+    int power_;
+};
+
 }
