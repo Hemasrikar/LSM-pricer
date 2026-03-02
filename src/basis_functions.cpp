d37cbaff0eb654e02a76d853edc969b65c8f3b9f
Hema Srikar <hemasrikar.ankem@gmail.com>
2026-03-02 14:32:05 +0000

    Added:Laguerre Polynomial

--- src/basis_functions.cpp
+++ src/basis_functions.cpp
@@ -37,4 +37,36 @@ private:
     int power_;
 };
 
+// Laguerre Polynomial Function
+
+class LaguerrePolynomial : public BasisFunction{
+public:
+    LaguerrePolynomial(int order) : order_(order) {
+    if (order < 0 || order > 5)
+        throw std::invalid_argument("LaguerrePolynomial: order must be 0 to 5");
+}
+    double LaguerrePolynomial::evaluate(double x) const {
+        if (x < 0.0) x = 0.0;  // guard against negative normalised prices
+        const double e = std::exp(-x / 2.0);
+        switch (order_) {
+            case 0: return e;
+            case 1: return e * (1.0 - x);
+            case 2: return e * (1.0 - 2.0*x + 0.5*pow(x, 2));
+            case 3: return e * (1.0 - 3.0*x + 1.5*pow(x, 2) - pow(x, 3)/6.0);
+            case 4: return e * (1.0 - 4.0*x + 3.0*pow(x, 2) - (2.0/3.0)*pow(x, 3)
+                                    + pow(x, 4)/24.0);
+            case 5: return e * (1.0 - 5.0*x + 5.0*pow(x, 2) - (5.0/3.0)*pow(x, 3)
+                                + (5.0/24.0)*pow(x, 4) - pow(x, 5)/120.0);
+            default: return 0.0;
+        }
+    }
+
+    std::string name() const {
+        return "Laguerre_L" + std::to_string(order_);
+    }
+
+private:
+    int order_;
+};
+
 }
