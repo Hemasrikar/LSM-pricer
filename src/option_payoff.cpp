9d2a70ab374be2d60a5e7bff4f9d2c3612a54074
Hema Srikar <hemasrikar.ankem@gmail.com>
2026-03-03 14:30:12 +0000

    Formated code

--- src/option_payoff.cpp
+++ src/option_payoff.cpp
@@ -1,60 +1,29 @@
-#include <cmath>
 #include <algorithm> 
-
 #include "option_payoff.hpp"
 
-// Payoff functions of both put and call options
-// Functions to check if the call and put payoff functions are in-the-money or out-the-money
-// Use bool to check if the payoff functions is in-the-money
-// Otherwise, it will return as false which implies it is out-the-money
-// K = strike price
-// S = underlying price
+// Put_payoff implementations
+Put_payoff::Put_payoff(double strike) : K(strike) {}
 
-class Put_payoff 
+double Put_payoff::payoff(double S) const 
 {
-private:
-    double K;  
+    return std::max(K - S, 0.0);
+}
 
-public:
-    Put_payoff(double strike) : K(strike) {}
-
-    double payoff(double S) const 
-    {
-        return std::max(K - S, 0.0);
-    }
-
-    bool InTheMoney_Put(double S) const
-    {
-        return S < K;
-    }
-};
-
-
-class Call_payoff 
+bool Put_payoff::InTheMoney_Put(double S) const
 {
-private:
-    double K;  
-
-public:
-    Call_payoff(double strike) : K(strike) {}
-
-    double payoff(double S) const 
-    {
-        return std::max(S - K, 0.0);
-    }
-
-    bool InTheMoney_Call(double S) const
-    {
-        return S > K;
-    }
-};
+    return S < K;
+}
 
 
+// Call_payoff implementations
+Call_payoff::Call_payoff(double strike) : K(strike) {}
 
+double Call_payoff::payoff(double S) const 
+{
+    return std::max(S - K, 0.0);
+}
 
-
-
-
-
-
-
+bool Call_payoff::InTheMoney_Call(double S) const
+{
+    return S > K;
+}
