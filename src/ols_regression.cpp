82fbef67887e10c911e58c0a4c20f8351364917f
Alex <Alexandrebanh@hotmail.com>
2026-03-04 12:44:02 +0000

    Expanded on framework of OLS_regression func.

---
+++ src/ols_regression.cpp
@@ -0,0 +1,60 @@
+#pragma once
+#include <functional>
+#include <cmath>
+#include <vector>
+#include <stdexcept>
+// For finding header files necessary used: https://libeigen.gitlab.io/eigen/docs-nightly/group__QuickRefPage.html
+#include <Eigen/Dense>
+#include "Eigen/StdVector"
+#include "mc_paths.hpp"
+#include "option_payoff.hpp"
+
+
+using basis_function = std::function<std::vector<double>(double)>; 
+class OLSRegression{
+// https://libeigen.gitlab.io/eigen/docs-nightly/group__TutorialMatrixClass.html#:~:text=typedef%20Matrix%3Cdouble%2C%20Dynamic%2C%20Dynamic%3E%20MatrixXd%3B
+Eigen::MatrixXd datapoints;
+
+// https://stackoverflow.com/questions/40852757/c-how-to-convert-stdvector-to-eigenmatrixxd
+
+private:
+// OLS regressors
+std::vector<double> ols_parameters;
+
+
+
+
+public:
+
+std::vector<double> Ols_regression(
+    stochastic_process process, 
+    double simulation_number, 
+    mc_sampling sampler
+    int number_of_intervals, 
+    basis_function basis,
+    double interval_length,
+)
+{ 
+    // compute length_of_output(basis)
+    std::vector<Eigen::MatrixXd> OLS_regressors_across_time(number_of_intervals, Eigen::MatrixXd(simulation_number, length_of_output(basis))); // (???) Is that how you size a vector of Eigen matrices???
+    std::vector<Eigen::MatrixXd> 
+    // Need to derive OLS regressors here noting that the design matrix will be made for each time. Since the basis function gives all Beta params
+    // Need to assign 
+    datapoints.resize(number_of_intervals, length_of_output(basis)); //https://libeigen.gitlab.io/eigen/docs-nightly/group__TutorialMatrixClass.html#:~:text=this%20page.-,Resizing,-The%20current%20size
+    std::vector<double> sampled_path;
+    for( i = 0; i < simulation_number; i++){
+        sampled_path = sampler(number_of_intervals, interval_length, process);
+        for(j = 0; j < interval_number; j++) {
+
+           datapoints(i, j) = basis(sampled_path[j]);
+
+        }
+    }
+
+
+
+
+}
+
+// Potential discussion on code performance: https://scicomp.stackexchange.com/questions/3159/is-it-a-good-idea-to-use-vectorvectordouble-to-form-a-matrix-class-for-high
+// 
