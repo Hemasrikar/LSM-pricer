0d1e0918ab7ef034e2e52abd654c434309d736be
Alex <Alexandrebanh@hotmail.com>
2026-03-14 14:33:51 +0000

    Added tests for buildDesignMatrix function (both for Laguerre and Monomial basis). Note second require test is intentionally made to fail since catch2 only displays couts if the test fails. This test output confirms that buildDesignMatrix works as intended.

--- tests/my_test.cpp
+++ tests/my_test.cpp
@@ -1,5 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "basis_functions.hpp"
+#include "ols_regression.hpp"
 
 TEST_CASE("0 == 0", "[tests]")
 {
@@ -9,4 +11,36 @@ TEST_CASE("0 == 0", "[tests]")
 TEST_CASE("1 + 1 == 2", "[tests]")
 {
 	REQUIRE(1 + 1 == 2);
+}
+
+
+TEST_CASE("OLS: DesignMatrixTest with Monomial Basis", "ols")
+{
+std::vector<double> S_t = {100, 110, 90, 95, 105};
+std::vector<bool> itm = {false, true, true, true, false};
+lms::BasisSet basis;
+basis.makeMonomialSet(3);
+
+Eigen::MatrixXd X = buildDesignMatrix(S_t, itm, basis);
+
+std::cout << "Design Matrix X: \n" << X << std::endl;
+
+REQUIRE(X.rows() == 3); // Only 3 in-the-money points
+REQUIRE(X.cols() == 5); // 4 basis functions (including constant)
+}
+
+
+TEST_CASE("OLS: DesignMatrixTest with Laguerre Polynomials", "ols1")
+{
+std::vector<double> S_t = {100, 110, 90, 95, 105};
+std::vector<bool> itm = {false, true, true, true, false};
+lms::BasisSet basis;
+basis.makeLaguerreSet(3);
+
+Eigen::MatrixXd X = buildDesignMatrix(S_t, itm, basis);
+
+std::cout << "Design Matrix X:\n" << X << std::endl;
+
+REQUIRE(X.rows() == 3); // Only 3 in-the-money points
+REQUIRE(X.cols() == 5); // 4 basis functions (including constant)
 }
