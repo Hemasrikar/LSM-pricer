0d6fa24573cbff9bbe2fd48405b6c89e8e3b7409
Bobbywsv <bobbywsv@gmail.com>
2026-03-14 19:01:01 +0000

    Added: Changes to the payoff & ITM functions

--- src/option_payoff.cpp
+++ src/option_payoff.cpp
@@ -1,29 +1,32 @@
 #include <algorithm> 
 #include "option_payoff.hpp"
 
+
+OptionPayoff::OptionPayoff(double strike) : K(strike) {}
+
 // Put_payoff implementations
-Put_payoff::Put_payoff(double strike) : K(strike) {}
+Put_payoff::Put_payoff(double strike) : OptionPayoff(strike) {}
 
 double Put_payoff::payoff(double S) const 
 {
     return std::max(K - S, 0.0);
 }
 
-bool Put_payoff::InTheMoney_Put(double S) const
+bool Put_payoff::InTheMoney(double S) const
 {
     return S < K;
 }
 
 
 // Call_payoff implementations
-Call_payoff::Call_payoff(double strike) : K(strike) {}
+Call_payoff::Call_payoff(double strike) : OptionPayoff(strike) {}
 
 double Call_payoff::payoff(double S) const 
 {
     return std::max(S - K, 0.0);
 }
 
-bool Call_payoff::InTheMoney_Call(double S) const
+bool Call_payoff::InTheMoney(double S) const
 {
     return S > K;
 }
