3eb7f34cb8a94baae77d8b1161ee1a1541f05793
Bobbywsv <bobbywsv@gmail.com>
2026-03-15 17:24:30 +0000

    Added:Included usage of namespace for payoff

--- src/option_payoff.cpp
+++ src/option_payoff.cpp
@@ -1,32 +1,37 @@
 #include <algorithm> 
 #include "option_payoff.hpp"
 
+namespace lsm {
+    namespace core {
+        
+        OptionPayoff::OptionPayoff(double strike) : K(strike) {}
 
-OptionPayoff::OptionPayoff(double strike) : K(strike) {}
+        // Put_payoff implementations
+        Put_payoff::Put_payoff(double strike) : OptionPayoff(strike) {}
 
-// Put_payoff implementations
-Put_payoff::Put_payoff(double strike) : OptionPayoff(strike) {}
+        double Put_payoff::payoff(double S) const 
+        {
+            return std::max(K - S, 0.0);
+        }
 
-double Put_payoff::payoff(double S) const 
-{
-    return std::max(K - S, 0.0);
-}
-
-bool Put_payoff::InTheMoney(double S) const
-{
-    return S < K;
-}
+        bool Put_payoff::InTheMoney(double S) const
+        {
+            return S < K;
+        }
 
 
-// Call_payoff implementations
-Call_payoff::Call_payoff(double strike) : OptionPayoff(strike) {}
+        // Call_payoff implementations
+        Call_payoff::Call_payoff(double strike) : OptionPayoff(strike) {}
 
-double Call_payoff::payoff(double S) const 
-{
-    return std::max(S - K, 0.0);
-}
+        double Call_payoff::payoff(double S) const 
+        {
+            return std::max(S - K, 0.0);
+        }
 
-bool Call_payoff::InTheMoney(double S) const
-{
-    return S > K;
-}
+        bool Call_payoff::InTheMoney(double S) const
+        {
+            return S > K;
+        }
+
+    } //namespace core
+} //namespace lsm
