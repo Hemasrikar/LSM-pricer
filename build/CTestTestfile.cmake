65707f1b3dfcc685a99d1a8072153d39b2f61c16
Hema Srikar <hemasrikar.ankem@gmail.com>
2026-03-22 15:39:10 +0000

    External files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/srikar/Documents/WBS/IB9JHO/Group Project/group-assignment-unicorn
+# Build directory: /Users/srikar/Documents/WBS/IB9JHO/Group Project/group-assignment-unicorn/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("tests")
---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/srikar/Documents/WBS/IB9JHO/Group Project/group-assignment-unicorn/tests
+# Build directory: /Users/srikar/Documents/WBS/IB9JHO/Group Project/group-assignment-unicorn/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(my_test "/Users/srikar/Documents/WBS/IB9JHO/Group Project/group-assignment-unicorn/build/tests/my_test")
+set_tests_properties(my_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/srikar/Documents/WBS/IB9JHO/Group Project/group-assignment-unicorn/tests/CMakeLists.txt;3;add_test;/Users/srikar/Documents/WBS/IB9JHO/Group Project/group-assignment-unicorn/tests/CMakeLists.txt;0;")
