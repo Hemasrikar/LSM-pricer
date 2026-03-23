913c9a06ae4a651682638f35a4073ca8fa7d6b51
Joanna Okenla <joannaokenla@Joannas-MacBook-Air-3.local>
2026-03-23 22:40:01 +0000

    created test for convergence analyser

---
+++ tests/test_convergence_analyser.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "convergence_analyser.hpp"
+
+static lsm::analysis::ConvergenceAnalyser analyser(100.0, 0.05, 0.2, 100.0, 1.0, true);
+
+
+using namespace lsm::analysis;
+
+TEST_CASE("getBSPrice is correct", "[convergence]") {
+    REQUIRE(analyser.getBSPrice() == Approx(10.4506).epsilon(0.001));
+}
